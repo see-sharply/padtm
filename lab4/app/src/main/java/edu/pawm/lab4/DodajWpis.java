008e8b7877328d6ae64b6054a19f2e6be737d2a8
co36010 <co36010@zut.edu.pl>
2021-05-23 01:07:30 +0200

    lab 4 - has been done

--- lab4/app/src/main/java/edu/pawm/lab4/DodajWpis.java
+++ lab4/app/src/main/java/edu/pawm/lab4/DodajWpis.java
@@ -2,7 +2,10 @@ package edu.pawm.lab4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class DodajWpis extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class DodajWpis extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dodaj_wpis);
+    }
+
+    public void wyslij(View view){
+        EditText kontrolka = (EditText)findViewById(R.id.editText);
+        String pole = kontrolka.getText().toString();
+        Intent interacja = new Intent();
+        interacja.putExtra("wpis", pole);
+        setResult(RESULT_OK, interacja);
+        finish();
     }
 }
--- lab4/app/src/main/java/edu/pawm/lab4/MainActivity.java
+++ lab4/app/src/main/java/edu/pawm/lab4/MainActivity.java
@@ -1,5 +1,6 @@
 package edu.pawm.lab4;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -46,5 +47,19 @@ public class MainActivity extends AppCompatActivity {
 
     public void nowyWpis(MenuItem mi){
         Intent intencja = new Intent(this, DodajWpis.class);
+        startActivityForResult(intencja, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode==1 && resultCode == RESULT_OK)
+        {
+            Bundle extras = data.getExtras();
+            String nowy = (String)extras.get("wpis");
+            target.add(nowy);
+            adapter.notifyDataSetChanged();
+        }
     }
 }
