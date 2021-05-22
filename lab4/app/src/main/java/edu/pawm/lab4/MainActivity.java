956d56d4dd2bf24a90a2156dff87d5f7bc162d10
co36010 <co36010@zut.edu.pl>
2021-05-22 20:01:30 +0200

    lab 4 - listview has been done

--- lab4/app/src/main/java/edu/pawm/lab4/MainActivity.java
+++ lab4/app/src/main/java/edu/pawm/lab4/MainActivity.java
@@ -3,12 +3,33 @@ package edu.pawm.lab4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.AdapterViewAnimator;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> target;
+    private ArrayAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] values = new String[]{
+                "Pies", "Kot", "Gołąb", "Kruk", "Dzik", "Karp", "Osioł", "Chomik", "Mysz", "Jeż", "Karaluch"
+        };
+
+        this.target = new ArrayList<String>();
+        this.target.addAll(Arrays.asList(values));
+
+        this.adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, this.target);
+
+        ListView listview = (ListView)findViewById(R.id.listView);
+        listview.setAdapter(this.adapter);
     }
 }
